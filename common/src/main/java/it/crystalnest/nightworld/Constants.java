bc954a58446c066dc76683877a70cd5f13ad58db
Crystal Spider <riccardo.chimisso@gmail.com>
2025-02-05 11:52:47 +0100

    Update for SSP 2.0.1.

--- common/src/main/java/it/crystalnest/nightworld/Constants.java
+++ common/src/main/java/it/crystalnest/nightworld/Constants.java
@@ -1,5 +1,9 @@
 package it.crystalnest.nightworld;
 
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.ApiStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +17,11 @@ public final class Constants {
    * Mod ID.
    */
   public static final String MOD_ID = "nightworld";
+
+  /**
+   * Nightworld dimension {@link ResourceKey}.
+   */
+  public static final ResourceKey<Level> NIGHTWORLD = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(MOD_ID, "nightworld"));
 
   /**
    * Mod logger.

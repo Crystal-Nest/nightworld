a6fd801fceb1dfc8991f44848a6ab18cb3dfd10a
Crystal Spider <riccardo.chimisso@gmail.com>
2025-02-05 11:52:47 +0100

    Update for SSP 2.0.1.

--- common/src/main/java/it/crystalnest/nightworld/mixin/NetherPortalBlockMixin.java
+++ common/src/main/java/it/crystalnest/nightworld/mixin/NetherPortalBlockMixin.java
@@ -9,12 +9,15 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.NetherPortalBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -22,7 +25,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  * Injects into {@link NetherPortalBlock} to alter Nightworld Portals mob spawn.
  */
 @Mixin(NetherPortalBlock.class)
-public abstract class NetherPortalBlockMixin {
+public abstract class NetherPortalBlockMixin extends Block {
+  private NetherPortalBlockMixin() {
+    super(BlockBehaviour.Properties.of(Material.PORTAL));
+  }
+
   /**
    * Injects into the method {@link NetherPortalBlock#randomTick(BlockState, ServerLevel, BlockPos, RandomSource)} before the call to {@link BlockState#isValidSpawn(BlockGetter, BlockPos, EntityType)}.<br />
    * Allows Zombies and Skeletons spawn when it's a Nightworld Portal.
@@ -33,14 +40,17 @@ public abstract class NetherPortalBlockMixin {
    * @param random random source.
    * @param ci {@link CallbackInfo}.
    */
-  @Inject(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isValidSpawn(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/EntityType;)Z", shift = Shift.BEFORE))
+  @Inject(method = "randomTick", at = @At(value = "HEAD"), cancellable = true)
   private void onRandomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random, CallbackInfo ci) {
-    if (CustomPortalChecker.isPortalForDimension(level, pos.above(), Constants.MOD_ID)) {
-      if (random.nextInt(0, 100) < 50) {
-        this.handleSpawnEntity(EntityType.ZOMBIE, level, pos);
+    if (level.getGameRules().getBoolean(GameRules.RULE_DOMOBSPAWNING) && random.nextInt(500) < level.getDifficulty().getId() && CustomPortalChecker.isPortalForDimension(level, pos.above(), Constants.NIGHTWORLD)) {
+      BlockPos bottom;
+      for (bottom = pos; level.getBlockState(bottom).is(this); bottom = bottom.below()) ;
+      if (random.nextInt(0, 10) < 5) {
+        this.handleSpawnEntity(EntityType.ZOMBIE, level, bottom);
       } else {
-        this.handleSpawnEntity(EntityType.SKELETON, level, pos);
+        this.handleSpawnEntity(EntityType.SKELETON, level, bottom);
       }
+      ci.cancel();
     }
   }
 
